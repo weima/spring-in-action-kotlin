3e7ed3a65cc685a926313ca2d16e5809e1180747
Wei Ma <weima.gm@gmail.com>
2019-01-25 11:42:39 -0800

    Completed tacocloud

---
+++ taco-cloud/src/main/kotlin/sia/tacocloud/controller/HelloController.kt
@@ -0,0 +1,12 @@
+package sia.tacocloud.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HelloController {
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "hello"
+    }
+}
--- taco-cloud/src/test/kotlin/sia/tacocloud/controller/HomeControllerTest.kt
+++ taco-cloud/src/test/kotlin/sia/tacocloud/controller/HomeControllerTest.kt
@@ -1,12 +1,29 @@
 package sia.tacocloud.controller
 
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.Assert.*
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @RunWith(SpringRunner::class)
 @WebMvcTest(HomeController::class)
-class HomeControllerTest{
+class HomeControllerTest {
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun testHomePage() {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk)
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to ...")))
+
+    }
 
 }
