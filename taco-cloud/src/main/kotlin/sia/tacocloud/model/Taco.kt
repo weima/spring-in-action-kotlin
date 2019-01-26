493b5f9df1fcc84bb93b3a13fb792c8c3076ac1a
Wei Ma <weima@localhost.localdomain>
2019-01-26 10:14:22 -0800

    fixed ingredient in Taco

--- taco-cloud/src/main/kotlin/sia/tacocloud/model/Taco.kt
+++ taco-cloud/src/main/kotlin/sia/tacocloud/model/Taco.kt
@@ -1,20 +1,18 @@
 package sia.tacocloud.model
 
-import lombok.Data
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
-@Data
-class Taco {
+data class Taco(
 
-    // end::allButValidation[]
-    @NotNull
-    @Size(min = 5, message = "Name must be at least 5 characters long")
-    private// tag::allButValidation[]
-    val name: String? = null
-    // end::allButValidation[]
-    @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private// tag::allButValidation[]
-    val ingredients: List<String>? = null
+        // end::allButValidation[]
+        @NotNull
+        @Size(min = 5, message = "Name must be at least 5 characters long")
+        // tag::allButValidation[]
+        val name: String? = null,
+        // end::allButValidation[]
+        @Size(min = 1, message = "You must choose at least 1 ingredient")
+        // tag::allButValidation[]
+        val ingredients: List<String>? = null
 
-}
+)
